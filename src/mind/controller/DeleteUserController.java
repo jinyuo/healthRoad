91007c62858581b909a37048355460891ef8d7b0
Jeongleeyop <anfdmavy777@naver.com>
2019-12-05 16:56:36 +0900

    -

---
+++ src/mind/controller/DeleteUserController.java
@@ -0,0 +1,18 @@
+package mind.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeleteUserController implements HealthController {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

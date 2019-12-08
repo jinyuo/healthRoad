cd13f642967dff0d89a6a03f2f521fbfce4311a2
leechungyul <delight8069@naver.com>
2019-12-08 17:24:33 +0900

    사업자 등록 할때 사진등록
    Signed-off-by: leechungyul <delight8069@naver.com>

--- src/mind/controller/GymInsertController.java
+++ src/mind/controller/GymInsertController.java
@@ -1,11 +1,16 @@
 package mind.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import mind.model.dto.GymDTO;
 import mind.service.HealthService;
@@ -19,25 +24,38 @@ public class GymInsertController implements HealthController {
 
 		String id = session.getAttribute("curUserId").toString();
 
-		String name = request.getParameter("gymName");
-		String addr = request.getParameter("addr");
-		String phoneNum = request.getParameter("phone");
-		int gymCapacity =Integer.parseInt(request.getParameter("capacity"));
-		int price = Integer.parseInt(request.getParameter("price"));
-		String comment = request.getParameter("comment");
-		String weekday = request.getParameter("weekday");
-		String weekend = request.getParameter("weekend");
-		String fileName = request.getParameter("fileName");
+		//멀티파트리퀘스트 생성에 필요한 매개변수들 설정
+		ServletContext application = request.getServletContext();
+		String saveDir=application.getRealPath("/save/gym");
+		String encoding = "UTF-8";
+		int maxSize=1024*1024*100; //100mb
 		
-		System.out.println(name);
-		System.out.println(addr);
-		System.out.println(phoneNum);
-		System.out.println(gymCapacity);
-		System.out.println(price);
-		System.out.println(comment);
-		System.out.println(weekday);
-		System.out.println(weekend);
-		System.out.println(fileName);
+		//멀티파트리퀘스트 생성
+		MultipartRequest m = new MultipartRequest(request, saveDir, maxSize, encoding, new DefaultFileRenamePolicy());
+		File file = m.getFile("file");
+		
+		
+		//String name = request.getParameter("gymName");
+		//String addr = request.getParameter("addr");
+		//String phoneNum = request.getParameter("phone");
+		//int gymCapacity =Integer.parseInt(request.getParameter("capacity"));
+		//int price = Integer.parseInt(request.getParameter("price"));
+		//String comment = request.getParameter("comment");
+		//String weekday = request.getParameter("weekday");
+		//String weekend = request.getParameter("weekend");
+		//String fileName = request.getParameter("fileName");
+		
+		String name = m.getParameter("gymName");
+		String addr = m.getParameter("addr");
+		String phoneNum =m.getParameter("phone");
+		int gymCapacity =Integer.parseInt(m.getParameter("capacity"));
+		int price = Integer.parseInt(m.getParameter("price"));
+		String comment = m.getParameter("comment");
+		String weekday = m.getParameter("weekday");
+		String weekend = m.getParameter("weekend");
+		String fileName = m.getOriginalFileName("file");
+		//System.out.println(name);
+		//System.out.println(fileName);
 		
 		GymDTO gym = new GymDTO(name,addr,phoneNum,gymCapacity,price,comment, weekday,weekend,fileName);
 		
